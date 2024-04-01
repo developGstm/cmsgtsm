1c0c7f0a2a8915f0e0df0e22fbda13d6ec93ba09
emmanuel aviles <eavilesp@aeromexico.com>
2024-04-01 14:09:11 -0600

    Cambios para destiny

---
+++ src/api/servicios-destiny/controllers/servicios-destiny.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * servicios-destiny controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::servicios-destiny.servicios-destiny');
